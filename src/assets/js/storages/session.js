e6ebd939af0abe0511e9cb90ba190a6af34e0607
evertonthepaula <evertothepaula@gmail.com>
2018-05-16 11:23:09 -0300

    Criar script para controlar sessionStorages.

---
+++ src/assets/js/storages/session.js
@@ -0,0 +1,22 @@
+export default (() => {
+  return {
+    setStorage(key,value){
+      return sessionStorage.setItem(key, value);
+    },
+
+    getStorage(key)
+    {
+      return sessionStorage.getItem(key);  
+    },
+
+    removeStorage(key)
+    {
+      return sessionStorage.removeItem(key);
+    },
+
+    clear(){
+      return sessionStorage.clear();
+    },
+  };
+
+})();
